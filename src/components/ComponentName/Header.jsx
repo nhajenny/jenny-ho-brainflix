a3e412da10a3a885b0ddb6535cc6060e62cd1839
Jenny Ho <nha.jennyho@gmail.com>
2024-10-27 17:39:23 -0400

    add styling for header components for mobile

--- src/components/ComponentName/Header.jsx
+++ src/components/ComponentName/Header.jsx
@@ -3,24 +3,31 @@ import "../../styles/styles.css";
 import logo from '../../assets/images/logo/BrainFlix-logo.svg';
 import iconUpload from '../../assets/images/icons/upload.svg';
 import mohanMuruge from '../../assets/images/Mohan-muruge.jpg';
+import iconSearch from '../../assets/images/icons/search.svg';
 
 function Header() {
-    <header className="header">
+    return (
+        <header>
         <div className="header__logo">
             <img src={logo} alt="BrainFlix logo" className="header__logo--img"/>
-            BrainFlix
         </div>
         <div className="header__navbar">
             <div className="header__search">
-                <input type="text" placeholder="Search" className="header__search--input"></input>
+                <div className="header__search--container">
+                <input type="text" placeholder="Search" className="header__search--input">
+                </input>
+                <img src={iconSearch} alt="search icon left side of search bar" className="header__search--icon"/>
+                </div>
                 <img src={mohanMuruge} alt="profile picture of Mohan Muruge" className="header__search--profile"/>
             </div>
-            <button className="header__button">UPLOAD
+            <button className="header__button">
                 <img src={iconUpload} alt="upload icon left side of button" className="header__button--upload-icon"/>
+                <label className="header__button--text">UPLOAD</label>
             </button>
             <img src={mohanMuruge} alt="profile picture of Mohan Muruge" className="header__navbar--profile"/>
         </div>
     </header>
+    )
 }
 
 export default Header;

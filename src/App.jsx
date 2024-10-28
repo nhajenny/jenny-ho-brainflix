c05df94e963a6558a5dfe0036d54cde854d47354
Jenny Ho <nha.jennyho@gmail.com>
2024-10-27 20:01:13 -0400

    add videoplayer component and integrate it into app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
+import { useEffect,useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/ComponentName/Header.jsx';
+import Videoplayer from './components/ComponentName/Videoplayer.jsx';
+import videoData from './data/video-details.json';
 
 function App() {
+const [videos, setVideos]=useState([]);
+useEffect(() => {
+  setVideos(videoData); 
+}, []);
+
   return (
     <div>
       <Header></Header>
+      <section>
+        {videos.length > 0 && (
+          <div className="video-selected">
+            <Videoplayer videoData={videos[0]}></Videoplayer>
+          </div>
+        )}
+      </section>
     </div>
   )
 }
--- src/components/ComponentName/Videoplayer.jsx
+++ src/components/ComponentName/Videoplayer.jsx
@@ -1,14 +1,17 @@
 import React from'react';
 import '../../styles/styles.css';
-import preview from "../../assets/images/Upload-video-preview.jpg";
 
-function Videoplayer () {
+
+function Videoplayer ({videoData}) {
     return (
         <div className="video__container">
-            <video className="video__player" src={videoUrl}
-            poster={preview}
-            controls type="video/mp4"></video>
+            <video className="video__player"
+            src={videoData.video}
+            poster={videoData.image}
+            controls
+            type="video/mp4">Your browser does not support video tag
+            </video>
         </div>
-    )
+    );
 }
 export default Videoplayer;

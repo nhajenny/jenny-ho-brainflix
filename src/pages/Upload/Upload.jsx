d7ae0dfd86a3babe971a1c8862e9b4262f33df1f
Jenny Ho <nha.jennyho@gmail.com>
2024-11-03 22:30:39 -0500

    add styling for upload page

--- src/pages/Upload/Upload.jsx
+++ src/pages/Upload/Upload.jsx
@@ -1,17 +1,18 @@
 import uploadImage from '../../assets/images/Upload-video-preview.jpg';
 import iconUpload from '../../assets/images/icons/upload.svg';
 import "../../styles/styles.css";
+import {Link} from 'react-router-dom';
 
 function Upload () {
     return(
         <section className="upload">
             <h1 className="upload__title">Upload Video</h1>
+            <div className="upload__subtitle">Video Thumbnail</div>
+            <div className="upload__desktop--container">
             <div>
-                <div className="upload__subtitle">Video Thumbnail</div>
                 <img src={uploadImage} alt="person at starting line of track run" className="upload__image" ></img>
             </div>
-            <div>
-                
+            <div> 
                 <form className="upload__form">
                     <label className="upload__form--label">TITLE YOUR VIDEO
                         <input 
@@ -28,17 +29,29 @@ function Upload () {
                         placeholder = "Add a description to your video"
                         className="upload__form--description"></textarea>
                     </label>
-                    <button 
-                    type="submit"
-                    className="upload__button">
-                        <img src={iconUpload} alt="upload icon" className="upload__button--icon"/>
-                        <label>Publish</label>
-                    </button>
                 </form>
-                <div className="upload__cancel">
-                    CANCEL
-                </div>
             </div>
+            </div>
+                    <div className="upload__bottom--buttons">
+                        <Link to="/" className="upload__cancel--link">
+                            <div className="upload__cancel">
+                                CANCEL
+                            </div>
+                        </Link>
+                        <Link to="/" className="upload__publish--link">
+                            <button 
+                                type="submit"
+                                className="upload__button">
+                                <img src={iconUpload} alt="upload icon" className="upload__button--icon"/>
+                                <label>PUBLISH</label>
+                            </button>
+                        </Link>
+                    </div>
+                <Link to="/" className="upload__cancel--link">
+                    <div className="upload__cancel--mobile">
+                        CANCEL
+                    </div>
+                </Link>
         </section>
     )
 }
